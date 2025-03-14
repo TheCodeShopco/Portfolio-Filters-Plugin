5c33dd92143d94fa43b22b2ea3e9cc36aa6d0e50
Alistair Everington <hello@thecodeshop.co>
2025-03-14 14:09:17 +0000

    Add files via upload

--- TCS_PortfolioFiltersPlugin.js
+++ TCS_PortfolioFiltersPlugin.js
@@ -1,25 +1,101 @@
 function initialisePortfolioFilters() {
 
-    function findFilterLocation() {
-        let filterBlock = document.getElementById('portfolio-filters');
-        if (!filterBlock) {
-            let grid = document.querySelector('#gridThumbs');
-            if (grid === null) {
-                return;
-            }
-            filterBlock = document.createElement('div');
-            filterBlock.id = 'portfolio-filters';
-            grid.parentNode.insertBefore(filterBlock, grid);
-            filterBlock.classList.add('filters-default');
-            grid.classList.add('filters-default-active');
-            return filterBlock;
-        } else {
-            return filterBlock;
+    function findPortfolioSection() {
+        let targetBlock = document.querySelector('#filtered-portfolio-section');
+        if (targetBlock) {
+            let portfolioSection = targetBlock.closest('section').nextElementSibling;
+            return portfolioSection;
         }
     }
 
-    function findPortfolioCategories() {
-        let grid = document.querySelector('#gridThumbs');
+    function addFilterComponents(portfolioSection) {
+        let targetBlock = document.querySelector('#filtered-portfolio-section');
+        if (targetBlock) {
+            let filterWrapper = document.createElement('div');
+            filterWrapper.id = 'portfolio-section-filter-wrapper';
+            filterWrapper.classList.add('sqs-block-form');
+            if (targetBlock.getAttribute('data-search-enabled') === 'true') {
+                // Creating the search bar and adding its classes/event listener
+                let searchBar = document.createElement('input');
+                searchBar.type = 'text';
+                searchBar.placeholder = 'Search items...';
+                searchBar.id = 'portfolio-section-search-bar';
+                searchBar.addEventListener('input', filterPortfolioSection);
+                // Creating the search bar wrapper and adding its classes and id
+                let searchBarWrapper = document.createElement('div');
+                searchBarWrapper.id = "portfolio-section-filters-search-bar-wrapper";
+                searchBarWrapper.classList.add('form-item', 'field', 'text');
+                // Adding the search bar to the wrapper
+                searchBarWrapper.appendChild(searchBar);
+                // Creating the form styling div and adding its classes and inner elements
+                let formStylings = document.createElement('span');
+                formStylings.classList.add('form-input-effects');
+                formStylings.innerHTML = '<span class="form-input-effects-border"></span>';
+                // Adding the form styling to the search bar wrapper
+                searchBarWrapper.appendChild(formStylings);
+                // Adding the search bar wrapper to the filter wrapper
+                filterWrapper.appendChild(searchBarWrapper);
+            }
+            // Adding the category select bar if it is enabled
+            if (targetBlock.getAttribute('data-categories-enabled') === 'true') {
+                // Creating the categories select bar and adding its classes/event listener
+                let selectBar = document.createElement('select');
+                selectBar.id = 'portfolio-section-select-bar';
+                selectBar.addEventListener('change', filterListSection);
+                // Creating the default option for the select bar
+                let defaultOption = document.createElement('option');
+                defaultOption.value = 'all';
+                defaultOption.innerText = 'All Categories';
+                selectBar.appendChild(defaultOption);
+                // Creating the category select wrapper and adding its classes and id
+                let categoriesWrapper = document.createElement('div');
+                categoriesWrapper.id = "portfolio-section-filters-categories-wrapper";
+                categoriesWrapper.classList.add('form-item', 'field', 'select');
+                // Adding the category select inner to the category select wrapper
+                categoriesWrapper.appendChild(selectBar);
+                // Creating the dropdown icon and adding it to the category select wrapper
+                let dropdownIcon = document.createElement('div');
+                dropdownIcon.classList.add('select-dropdown-icon');
+                dropdownIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="12"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.439453 1.49825L1.56057 0.501709L9.00001 8.87108L16.4395 0.501709L17.5606 1.49825L9.00001 11.1289L0.439453 1.49825Z"></path></svg>';
+                categoriesWrapper.appendChild(dropdownIcon);
+                // Creating the form styling div and adding its classes and inner elements
+                let formStylings = document.createElement('span');
+                formStylings.classList.add('form-input-effects');
+                formStylings.innerHTML = '<span class="form-input-effects-border"></span>';
+                // Adding the form styling to the category select wrapper
+                categoriesWrapper.appendChild(formStylings);
+                // Adding the category select wrapper to the filter wrapper
+                filterWrapper.appendChild(categoriesWrapper);
+            }
+
+            // Adding the inset class if required
+            portfolioSection.querySelector('.content').insertBefore(filterWrapper, portfolioSection.querySelector('.content').firstChild);
+            if (document.body.classList.contains('tweak-portfolio-grid-basic-width-inset')) {
+                filterWrapper.classList.add('inset');
+            }
+
+            // Aligning the components depending on user selection
+            let alignment = targetBlock.getAttribute('data-horizontal-alignment');
+            switch (alignment) {
+                case 'center': {
+                    filterWrapper.style.justifyContent = 'center';
+                    break;
+                }
+                case 'right': {
+                    filterWrapper.style.justifyContent = 'flex-end';
+                    break;
+                }
+                case 'left': {
+                    filterWrapper.style.justifyContent = 'flex-start';
+                }
+            }
+            let spacing = targetBlock.getAttribute('data-bottom-margin');
+            filterWrapper.style.marginBottom = spacing;
+        }
+    }
+
+    function findCategories(portfolioSection) {
+        let grid = portfolioSection.querySelector('#gridThumbs');
         if (grid === null) {
             return;
         }
@@ -32,6 +108,7 @@ function initialisePortfolioFilters() {
             if (href) {
                 href = href.substring(1).split('/').slice(1).join('/');
                 let category = href.split('/')[0];
+                item.setAttribute('data-category', category);
                 categories.add(category);
             }
         }
@@ -40,93 +117,64 @@ function initialisePortfolioFilters() {
         return uniqueCategories;
     }
 
-    function createFilterButtons() {
-        let filterBlock = findFilterLocation();
-        if (!filterBlock) {
-            return; 
-        }
-        let categories = findPortfolioCategories();
+    // Helper function to add the categories to the select bar
 
-        let filterAll = document.createElement('a');
-        filterAll.classList.add('filter-all', 'active');
-        filterAll.innerHTML = 'All';
-        filterAll.addEventListener('click', () => filterPortfolioItems(filterAll));
-        filterBlock.appendChild(filterAll);
-
-        for (let category of categories) {
-            let filterLink = document.createElement('a');
-            filterLink.classList.add(`filter-${category}`);
-            // Replace '-' with ' ' and capitalize the first letter of each word
-            let formattedCategory = category.replace(/-/g, ' ').replace(/\b\w/g, char => char.toUpperCase());
-            filterLink.innerHTML = formattedCategory;
-            filterLink.addEventListener('click', () => filterPortfolioItems(filterLink));
-            filterBlock.appendChild(filterLink);
-        }
+    function addCategoryOptions(categories) {
+        let selectBar = document.querySelector('#portfolio-section-select-bar');
+        if (!selectBar) return;
+        categories.forEach(category => {
+            let option = document.createElement('option');
+            option.value = category;
+            option.innerText = category;
+            selectBar.appendChild(option);
+        });
     }
 
-    function filterPortfolioItems(element) {
-        let filterCategory = element.classList[0].split('-').slice(1).join('-');
+    function filterPortfolioSection() {
+        let portfolioSection = findPortfolioSection();
+        let searchBar = document.querySelector('#portfolio-section-search-bar');
+        let selectBar = document.querySelector('#portfolio-section-select-bar');
 
-        document.querySelectorAll('#portfolio-filters a').forEach(filter => {
-            filter.classList.remove('active');
-        });
-        element.classList.add('active');
+        // Finding the values of the search and category bars
+        let searchQuery = searchBar ? searchBar.value.toLowerCase() : '';
+        let categoryQuery = selectBar ? selectBar.value : 'all';
 
-        document.querySelectorAll('#gridThumbs a').forEach(item => {
+        let portfolioItems = portfolioSection.querySelectorAll('.grid-item');
+
+        // Iterating over the list items and hiding them if they dont match the search criteria
+        portfolioItems.forEach(item => {
+            let itemName = item.querySelector('.portfolio-title').innerText.toLowerCase();
+            let itemCategory = item.getAttribute('data-category');
+
+            // Check if the search query matches the name, description, or any category
+            const matchesSearch = searchBar ? (
+            itemName.includes(searchQuery) ||
+            itemCategory.toLowerCase().includes(searchQuery)
+            ) : true;
+
+            // Check if the item matches the selected category
+            const matchesCategory = selectBar ? (categoryQuery === 'all' || itemCategory === categoryQuery) : true;
 
             item.classList.remove('visible');
             setTimeout(() => {
                 item.classList.add('hidden');
             }, 250);
-
+            
             setTimeout(() => {
-                if (filterCategory === 'all' || item.getAttribute('href').includes(filterCategory)) {
+                if (matchesSearch && matchesCategory) {
                     item.classList.remove('hidden');
                     setTimeout(() => {
                         item.classList.add('visible');
                     }, 10);
                 }
             }, 250);
-
         });
     }
 
-    createFilterButtons();
-
-    function assignCustomStyles() {
-        let filterBlock = findFilterLocation();
-        if (!filterBlock) {
-            return
-        }
-
-        if (filterBlock.classList.contains('filters-default')) {
-            return;
-        }
-
-        let alignment = filterBlock.getAttribute('data-alignment') || 'center';
-        let spacing = filterBlock.getAttribute('data-spacing') || '2rem';
-        let direction = filterBlock.getAttribute('data-row-or-column') || 'row';
-
-        switch (alignment) {
-            case 'center':
-                alignment = 'center';
-                break;
-            case 'left':
-                alignment = 'flex-start';
-                break;
-            case 'right':
-                alignment = 'flex-end';
-                break;
-            default:
-                alignment = 'center';
-        }
-
-        filterBlock.style.justifyContent = alignment;
-        filterBlock.style.gap = spacing;
-        filterBlock.style.flexDirection = direction;
-    }
-
-    assignCustomStyles();
+    let portfolioSection = findPortfolioSection();
+    let categories = findCategories(portfolioSection);
+    addFilterComponents(portfolioSection);
+    addCategoryOptions(categories);
     
 }
 
